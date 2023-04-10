27ea6fa49c9ec064f1fc1eaa7b30ba4aedf131ed
Sergey Bushelenkov <bsn1980@yandex.ru>
2023-04-10 21:18:47 +0300

    Добавил изменение к 2 задаче к 1 ДЗ

--- homework/homework1/task2/main.py
+++ homework/homework1/task2/main.py
@@ -1,11 +1,12 @@
 import math
 CraneSum = int(input('введите общее количество журавликов: '))
 
-Serg = math.ceil(CraneSum / 6)
+Point = CraneSum / 6
+Serg = math.ceil(Point)
 
 Petya = Serg
 
-Kate = (Serg + Petya) * 2
+Kate = math.floor(Point * 4)
 
 print(f"Петя сделал {Petya} журавликов, Сергей сделал {Serg} журавликов, Катя сделала {Kate} журавликов")
 
